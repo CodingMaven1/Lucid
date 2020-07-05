b4ce323bd990f8ee427c58d4048d163e15cc2dd1
Naman Agarwal <namana019@outlook.com>
2020-07-06 02:24:34 +0530

    Central Authority contract almost done

--- src/ethereum/contracts/CentralAuthority.sol
+++ src/ethereum/contracts/CentralAuthority.sol
@@ -6,35 +6,57 @@ contract CentralAuth {
         string Name;
         string Description;
         uint Price;
-        bool approval;
+        bool Approval;
     }
+    
     struct Company{
         string Name;
         string Description;
         uint ProductCount;
+        address Owner;
         mapping (uint => Product) Products;
     }
-
+    
     address public Admin;
     mapping (address => Company) public Companies;
-
+    
     constructor() public{
         Admin = msg.sender;
     }    
-
-    modifier restricted() {
+    
+    modifier onlyAdmin() {
         require(msg.sender == Admin, "Only Admin has the access");
         _;
     }
     
-    function CreateCompany(string memory _name, string memory  _description, address _companyowner) public restricted {
+    function CreateCompany(string memory _name, string memory  _description, address _companyowner) public onlyAdmin {
         Company memory newCompany = Company({
             Name: _name,
             Description: _description,
+            Owner: _companyowner,
             ProductCount: 0
         });
         
         Companies[_companyowner] = newCompany;
+    }
+    
+    function CreateProduct(string memory _name, string memory _Description, uint _price) public {
+        require(msg.sender == Companies[msg.sender].Owner, "Only Company Owner has the access");
+        Company storage CreatedCompany = Companies[msg.sender];
+        Product memory newProduct = Product({
+            Name: _name,
+            Description: _Description,
+            Price: _price,
+            Approval: false
+        });
+        
+        CreatedCompany.Products[CreatedCompany.ProductCount] = newProduct;
+        CreatedCompany.ProductCount++;
+    }
+    
+    function ApproveProduct(uint _index, address _companyowner) public onlyAdmin{
+        require(msg.sender == Companies[_companyowner].Owner, "You are not the Owner");
+        Companies[_companyowner].Products[_index].Approval = true;
     }
     
 }
