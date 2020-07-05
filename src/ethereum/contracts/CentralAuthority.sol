630a73c7c4fd1453053f7f593828be2b927f69c2
Naman Agarwal <namana019@outlook.com>
2020-07-05 19:13:30 +0530

    Function for adding a company

--- src/ethereum/contracts/CentralAuthority.sol
+++ src/ethereum/contracts/CentralAuthority.sol
@@ -8,19 +8,33 @@ contract CentralAuth {
         uint Price;
         bool approval;
     }
-    
     struct Company{
         string Name;
         string Description;
-        Product[] Products;
+        uint ProductCount;
+        mapping (uint => Product) Products;
     }
-    
+
     address public Admin;
     mapping (address => Company) public Companies;
-    
+
     constructor() public{
         Admin = msg.sender;
     }    
+
+    modifier restricted() {
+        require(msg.sender == Admin, "Only Admin has the access");
+        _;
+    }
     
-    // function CreateCompany(address)
+    function CreateCompany(string memory _name, string memory  _description, address _companyowner) public restricted {
+        Company memory newCompany = Company({
+            Name: _name,
+            Description: _description,
+            ProductCount: 0
+        });
+        
+        Companies[_companyowner] = newCompany;
+    }
+    
 }
