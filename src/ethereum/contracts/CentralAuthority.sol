52cdd5836b7cc44426c24a8228feeb2467bc3dff
Naman Agarwal <namana019@outlook.com>
2020-07-09 19:27:35 +0530

    Working on bidding part

--- src/ethereum/contracts/CentralAuthority.sol
+++ src/ethereum/contracts/CentralAuthority.sol
@@ -7,8 +7,10 @@ contract CentralAuth {
         string Description;
         uint Price;
         bool Approval;
+        uint Maxfees;
         uint BidCount;
-        mapping (uint => uint) Bids;
+        bool AcceptBid;
+        mapping (uint => Bid) Bids;
     }
     
     struct Company{
@@ -19,9 +21,16 @@ contract CentralAuth {
         mapping (uint => Product) Products;
     }
     
+    struct Bid{
+        uint demandfees;
+        uint security;
+        address logisticAddress;
+    }
+    
     struct Logistic{
         string Name;
         uint Rating;
+        address Owner;
     }
     
     address public Admin;
@@ -40,6 +49,7 @@ contract CentralAuth {
     }
     
     function CreateCompany(string memory _name, string memory  _description) public {
+        require(Companies[msg.sender].Owner == msg.sender, "Company already registered from this address");
         Company memory newCompany = Company({
             Name: _name,
             Description: _description,
@@ -50,7 +60,7 @@ contract CentralAuth {
         Companies[msg.sender] = newCompany;
     }
     
-    function CreateProduct(string memory _name, string memory _Description, uint _price) public {
+    function CreateProduct(string memory _name, string memory _Description, uint _price ) public {
         require(msg.sender == Companies[msg.sender].Owner, "Only Company Owner has the access");
         Company storage CreatedCompany = Companies[msg.sender];
         Product memory newProduct = Product({
@@ -58,7 +68,9 @@ contract CentralAuth {
             Description: _Description,
             Price: _price,
             BidCount: 0,
-            Approval: false
+            Approval: false,
+            Maxfees: 0,
+            AcceptBid: false
         });
         
         CreatedCompany.Products[CreatedCompany.ProductCount] = newProduct;
@@ -66,17 +78,27 @@ contract CentralAuth {
     }
     
     function ApproveProduct(uint _index, address _companyowner) public onlyAdmin{
-        require(msg.sender == Companies[_companyowner].Owner, "You are not the Owner");
         Companies[_companyowner].Products[_index].Approval = true;
     }
     
     function RegisterLogistics(string memory _name) public {
+        require(Logistics[msg.sender].Owner == msg.sender, "Logistic already registered from this address");
         Logistic memory newLogistics = Logistic({
             Name: _name,
-            Rating: 0
+            Rating: 0,
+            Owner: msg.sender
         });
         LogisticAddress.push(msg.sender);
         Logistics[msg.sender] = newLogistics;
+    }
+    
+    function AcceptBid(uint _maxfees, uint _index) public {
+        require(msg.sender == Companies[msg.sender].Owner, "Only Company Owner has the access");
+        Companies[msg.sender].Products[_index].AcceptBid = true;
+        Companies[msg.sender].Products[_index].Maxfees = _maxfees;
+    }
+    
+    function MakeBid(uint _demand) public payable {
         
     }
     
